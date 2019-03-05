209b3fc4d91acb61cf3009b1a6bf83f164bc2af3
沈星繁 <ikesnowy@outlook.com>
2019-03-05 20:24:42 +0800

    汉化标题和超链接

--- TimeLineBlog/Models/Article.cs
+++ TimeLineBlog/Models/Article.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace TimeLineBlog.Models
         /// <summary>
         /// 文章标题。
         /// </summary>
+        [Display(Name = "标题")]
         [StringLength(15, MinimumLength = 3)]
         [Required]
         public string Title { get; set; }
@@ -26,23 +28,27 @@ namespace TimeLineBlog.Models
         /// <summary>
         /// 文章的创建时间。
         /// </summary>
+        [Display(Name = "创建时间")]
         [DataType(DataType.DateTime)]
         public DateTime CreateTime { get; set; }
 
         /// <summary>
         /// 文章的最后修改时间。
         /// </summary>
+        [Display(Name = "最后修改")]
         [DataType(DataType.DateTime)]
         public DateTime LastModifyTime { get; set; }
 
         /// <summary>
         /// 文章的 Markdown 内容。
         /// </summary>
+        [Display(Name = "正文（Markdown）")]
         public string MarkdownContent { get; set; }
 
         /// <summary>
         /// 文章的 HTML 内容。
         /// </summary>
+        [Display(Name = "正文内容")]
         public string HTMLContent { get; set; }
     }
 }
