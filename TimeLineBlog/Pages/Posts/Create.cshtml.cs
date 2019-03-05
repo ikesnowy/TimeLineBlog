01421b99b3fbdd808d5b1d89de007b477e21ae9e
沈星繁 <ikesnowy@outlook.com>
2019-03-05 15:02:39 +0800

    Add Article's CRUD Pages

---
+++ TimeLineBlog/Pages/Posts/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TimeLineBlog.Models;
+using TimeLineBlog.Pages.Models;
+
+namespace TimeLineBlog.Pages.Posts
+{
+    public class CreateModel : PageModel
+    {
+        private readonly TimeLineBlog.Models.TimeLineBlogContext _context;
+
+        public CreateModel(TimeLineBlog.Models.TimeLineBlogContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Article Article { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Article.Add(Article);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
