3cce81d9fb7d100ccc6deeec3b82e328a3f10aa2
沈星繁 <ikesnowy@outlook.com>
2019-03-06 16:42:06 +0800

    Add Timeline Index and ReadArticle Page

--- TimeLineBlog/Controllers/HomeController.cs
+++ TimeLineBlog/Controllers/HomeController.cs
@@ -4,15 +4,41 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TimeLineBlog.Models;
 
 namespace TimeLineBlog.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly TimeLineBlogContext _context;
+
+        public HomeController(TimeLineBlogContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Article.OrderByDescending(i => i.CreateTime).ToList());
+        }
+
+        // GET: Home/ReadArticle/5
+        public async Task<IActionResult> ReadArticle(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = await _context.Article
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return View(article);
         }
 
         public IActionResult Privacy()
